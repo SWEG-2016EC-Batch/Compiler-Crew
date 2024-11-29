ad89a51e66215c09852e8b30322a230216e86305
SEFEFE DAGNE ABEBE <sefefedagne6@gmail.com>
2024-11-29 21:21:19 +0300

    Update Number_miles.cpp

--- ETS1252_Sefefe_Dagne/Number_miles.cpp
+++ ETS1252_Sefefe_Dagne/Number_miles.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    double tank_Capacity, miles_PerGallon, total_Miles;
+
+    // Input the tank capacity of your automobile
+    cout << "Enter the fuel tank capacity in gallons that your automobile can hold: "<<endl;
+    cin >> tank_Capacity;
+
+    // Input miles per gallon that your automobile can move
+    cout << "Enter the miles per gallon that your automobile can travel: ";
+    cin >> miles_PerGallon;
+
+    // Calculate the total miles that your automobile can move
+    total_Miles = tank_Capacity * miles_PerGallon;
+
+    
+    cout << "You can drive your automobile for about  " << total_Miles << " miles without refueling." << endl;
+
+    return 0;
+}
