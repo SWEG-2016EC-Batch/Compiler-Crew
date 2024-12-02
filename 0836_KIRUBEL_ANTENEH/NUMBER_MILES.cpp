fe8e80d133db3bda6dabc0c935a01074ca93bcae
KIRUBEL ANTENEH ABEBE <KIRUBEL.ANTENEH@AASTUSTUDENT.EDU.ET>
2024-12-02 11:22:48 -0800

    Update NUMBER_MILES.cpp

--- 0836_KIRUBEL_ANTENEH/NUMBER_MILES.cpp
+++ 0836_KIRUBEL_ANTENEH/NUMBER_MILES.cpp
@@ -1,1 +1,25 @@
+/* Write a program that prompts the capacity in gallon of an automobile fuel tank and the miles per gallons the automobile can 
+be driven . The program outputs the number miles the automobile can be driven without refueling.*/
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    double tank_Capacity, miles_PerGallon, total_Miles;
+
+    // Input the tank capacity of your automobile
+    cout << "Enter the fuel tank capacity in gallons that your automobile can hold: "<<endl;
+    cin >> tank_Capacity;
+
+    // Input miles per gallon that your automobile can move
+    cout << "Enter the miles per gallon that your automobile can travel: ";
+    cin >> miles_PerGallon;
+
+    // Calculate the total miles that your automobile can move
+    total_Miles = tank_Capacity * miles_PerGallon;
+
+    
+    cout << "You can drive your automobile for about  " << total_Miles << " miles without refueling." << endl;
+
+    return 0;
+}
