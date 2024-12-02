f59c83dbae9cdae6f38c60ef0fd1c8afa44c43b1
KIRUBEL ANTENEH ABEBE <KIRUBEL.ANTENEH@AASTUSTUDENT.EDU.ET>
2024-12-02 12:13:47 -0800

    Update BMI_calculator.cpp

--- 0836_KIRUBEL_ANTENEH/BMI_calculator.cpp
+++ 0836_KIRUBEL_ANTENEH/BMI_calculator.cpp
@@ -1,32 +1,47 @@
   /* Make a program that calculates the Body Mass Index (BMI) of a person and determine whether the person is in normal weight, underweight or overweight.
   BMI is calculated as (BMI = w/h*h). Make the program to work for multiple person before terminating.*/
-#include <iostream>
+  #include <iostream>
 using namespace std;
 
-int main() {
-    double weight, height, bmi;
-    char decision;
+int main(){
+    float user_weight, user_height, bmi;
+    char choice = 'y';
 
-    do {
-        cout << "can you Enter your weight (kg)? "<<endl;
-        cin >> weight;
-        cout << "can you Enter your height (m)? "<<endl;
-        cin >> height;
+    while (choice == 'y'){
+        line9:
+        cout << "Enter weight in kg: ";
+        cin >> user_weight;
+        if (cin.fail() || user_weight <= 0){
+            cout << "Invalid input please try again!\n";
+            cin.clear();
+            cin.ignore();
+            goto line9;
+        }
 
-        bmi = weight / (height * height);
-        cout << "Your BMI is: " << bmi << endl;
+        line20:
+        cout << "Enter height in Meter: ";
+        cin >> user_height;
 
-        if (bmi < 18.5)
-         { cout << "you are: Underweight" << endl;}
-        else if (bmi < 24.9)
-         { cout << "you are: Normal weight" << endl;}
-        else { cout << "you are: Overweight" << endl;}
-
-        cout << "Do you want to proceed? (y/n): ";
-        cin >> decision;
+        if (cin.fail() || user_height <= 0){
+            cout << "Invalid input please try again!\n";
+            cin.clear();
+            cin.ignore();
+            continue;
+        }
         
+        float height_squared = user_height * user_height;
+        bmi = user_weight / height_squared;
 
-    } while (decision == 'y' || decision == 'Y');
-
+        cout << "your BMi is: " << bmi << endl;
+        if (bmi < 18.5){
+            cout << "you are under weight! \n \n";
+        }else if (bmi > 25){
+            cout << "you are over weight! \n \n";
+        }else{
+            cout << "you have a normal weight! \n \n";
+        }
+        cout << "Do you want to continue?\ny for yes | other key for no: ";
+        cin >> choice;
+    }
     return 0;
 }
