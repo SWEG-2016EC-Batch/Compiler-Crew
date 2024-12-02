d818680e93898dcdd21072bc7c3fa00cc467e3f9
KIRUBEL ANTENEH ABEBE <KIRUBEL.ANTENEH@AASTUSTUDENT.EDU.ET>
2024-12-02 11:29:07 -0800

    Update result.cpp

--- 0836_KIRUBEL_ANTENEH/result.cpp
+++ 0836_KIRUBEL_ANTENEH/result.cpp
@@ -1,1 +1,26 @@
+/* write a program that finds the result of the expression x raised to y where the value of x and y are entered by the user .*/
 
+#include <iostream>
+#include <cmath> 
+
+using namespace std;
+
+int main() {
+    double x, y, final_result; // variable declaration
+
+    // specify the base of the question to be calculated
+    cout << "Enter the base of the number (x): "<<endl;
+    cin >> x;
+
+    // specify the power raised to the base x
+    cout << "please insert the exponent (y): "<<endl;
+    cin >> y;
+
+    // now calculate the result of x raised to the power of y
+   final_result = pow(x, y);
+
+    // Output the final result
+    cout << "The final result of " << x << " raised to the power of " << y << " is: " << final_result << endl;
+    cout <<"Thank you,sir";
+    return 0;
+}
