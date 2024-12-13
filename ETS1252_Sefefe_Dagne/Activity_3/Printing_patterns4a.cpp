d54eebdb976b594259ac06067a13a320077842e7
SEFEFE DAGNE ABEBE <sefefedagne6@gmail.com>
2024-12-13 09:48:18 +0300

    Update Printing_patterns4a.cpp

--- ETS1252_Sefefe_Dagne/Activity_3/Printing_patterns4a.cpp
+++ ETS1252_Sefefe_Dagne/Activity_3/Printing_patterns4a.cpp
@@ -1,1 +1,39 @@
+/*Write a program that generates the different patterns given.*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    //This is the first pattern
+    cout << "Pattern 1: A 5x5 grid of numbers from 1 to 5\n";
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= 5; j++) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    // This is the second pattern
+    cout << "Pattern 2: A 4x6 grid of letters from A to X\n";
+    char letter = 'A';
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 6; j++) {
+            cout << letter << " ";
+            letter++;
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    // This is the third pattern
+    cout << "Pattern 3: A right-angled triangle of numbers\n";
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
 
